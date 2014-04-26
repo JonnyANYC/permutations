45afb6273a5942c846db8f7070ed65912baa1617
JonnyA <jon@angelajonhome.com>
2014-04-26 13:05:57 -0700

    Removed the bulk generation of permutations, since it was almost always worse than incremental calculations.

--- src/com/angelajonhome/algorithms/Permutations.java
+++ src/com/angelajonhome/algorithms/Permutations.java
@@ -10,8 +10,6 @@ import java.util.Set;
 
 /**
  * 
- * TODO Implement a second algorithm that calculates each permutation separately, to keep memory complexity low 
- * (at the expense of time complexity). Necessary for sets > ~12.
  * 
  * @param <enumeration>
  */
@@ -19,12 +17,11 @@ public class Permutations<T> implements Iterable<List<T>>, Iterator<List<T>> {
 
 	private final List<T> inputSet;
 	private final int inputSetSize;
-	private List<List<T>> permutations; 
 
 	public Permutations( Set<T> set ) {
+		// FIXME Handle a null input set.
 		this.inputSet = new ArrayList<T>( set );
 		this.inputSetSize = set.size();
-		this.permutations = new ArrayList<List<T>>();
 	}
 
 	public Permutations( List<T> set ) {
@@ -35,50 +32,34 @@ public class Permutations<T> implements Iterable<List<T>>, Iterator<List<T>> {
 			this.inputSet = new ArrayList<T>( set );			
 		}
 		this.inputSetSize = set.size();
-		this.permutations = new ArrayList<List<T>>();
 	}
 
 	public Permutations( T[] enumeration ) {
-
+		// FIXME Handle a null input set.
 		List<T>set = new ArrayList<T>( Arrays.asList( enumeration ) );
 		this.inputSet = set;			
 		this.inputSetSize = set.size();
-		this.permutations = new ArrayList<List<T>>();
 	}
 
-	private int currentPathIndex = -1;
-	private boolean generateOnDemand = false;
-
-	public void generateOnDemand( boolean generateOnDemand ) { 
-		this.generateOnDemand = generateOnDemand;
-	}
-
-	
 	@Override
 	public Iterator<List<T>> iterator() {		
 
-		// FIXME Handle a null input set.
-
-		if ( !(inputSetSize > 0) ) { 
+		if ( inputSet == null || !(inputSetSize > 0) ) { 
 			return new ArrayList<List<T>>().iterator();
 		}
 
-		if ( !generateOnDemand ) {
-			// Calculate all permutations, store in a local List of Lists, and then return an iterator for the list.
-			generatePermutations( null, null );
-			return permutations.iterator();
-		} 
-
 		// Return this instance as the iterator. We'll calculate the permutations one at a time.
-		// TODO Log a warning if the set size is greater than 12.
 		return this;		
 	}
+
+	private int currentPathIndex = -1;
 
 	public List<T> next() { 
 		return generateNextPermutation(++currentPathIndex, null, null, -1);
 	}
 	
 	public boolean hasNext() {
+		// consider storing this internally, if the calculation is expensive.
 		return ( currentPathIndex < factorial(inputSetSize) -1 );
 	}
 	
@@ -86,32 +67,6 @@ public class Permutations<T> implements Iterable<List<T>>, Iterator<List<T>> {
 		throw new UnsupportedOperationException("The Permutations class doesn't support modifications to the source set or the calculated permutations."); 
 	}
 	
-	private void generatePermutations( List<T> currentPath, LinkedList<T> available ) {
-
-		if ( currentPath == null || available == null) {
-			currentPath = new ArrayList<T>( inputSetSize );
-			available = new LinkedList<T>( inputSet );
-		}
-
-		for ( int i = 0; i < available.size(); i++ ) { 
-
-			List<T> branchPath = new ArrayList<T>( inputSetSize );
-			branchPath.addAll( currentPath );
-
-			LinkedList<T> branchAvailable = new LinkedList<T>( available );
-			branchPath.add( branchAvailable.remove(i) );
-
-			if ( branchAvailable.size() == 0 ) {
-				// Found a complete path.
-				permutations.add( branchPath );
-
-			} else {
-				// Keep looking.
-				generatePermutations( branchPath, branchAvailable );
-			}
-		}
-	}
-
 	private List<T> generateNextPermutation( int pathIndexToBuild, List<T> currentPath, LinkedList<T> available, int remainder ) {
 
 		if ( currentPath == null || available == null ) {
--- test/src/com/angelajonhome/algorithms/PermutationsTest.java
+++ test/src/com/angelajonhome/algorithms/PermutationsTest.java
@@ -50,27 +50,8 @@ public class PermutationsTest {
 		set.add("banana");
 		set.add("canteloupe");
 		set.add("durian");
-		
-		Permutations<String> permutations = new Permutations<String>( set );
-
-		int count = 0;
-		for ( List<String> permutation : permutations ) { 
-			count++;
-		}
-		
-		assertEquals(count, 24);  // 4! = 24
-	}
-
-	@Test
-	public void list4OnDemandTest() {
-		List<String> set = new ArrayList<String>(4);
-		set.add("apple");
-		set.add("banana");
-		set.add("canteloupe");
-		set.add("durian");
 
 		Permutations<String> permutations = new Permutations<String>( set ); 
-		permutations.generateOnDemand(true);
 
 		int count = 0;
 		for ( List<String> permutation : permutations ) { 
@@ -81,221 +62,6 @@ public class PermutationsTest {
 		assertEquals(count, 24);  // 4! = 24
 	}
 
-	
-		@Test
-	public void list6Test() {
-		// With more than 10-12 it will probably hit an OutOfMemoryError.
-		List<String> items = new ArrayList<String>(6);
-		items.add("apple");
-		items.add("banana");
-		items.add("mango");
-		items.add("pear");
-		items.add("orange");
-		items.add("cherry");
-
-		Permutations<String> permutations = new Permutations<String>( items );
-
-		int count = 0;
-		for ( List<String> permutation : permutations ) { 
-			count++;
-		}
-		
-		assertEquals(count, 720);  // 6! = 720
-	}
-
-		@Test
-	public void list6OnDemandTest() {
-		List<String> items = new ArrayList<String>(6);
-		items.add("apple");
-		items.add("banana");
-		items.add("mango");
-		items.add("pear");
-		items.add("orange");
-		items.add("cherry");
-
-		Permutations<String> permutations = new Permutations<String>( items );
-		permutations.generateOnDemand(true);
-
-		int count = 0;
-		for ( List<String> permutation : permutations ) { 
-			count++;
-		}
-		
-		assertEquals(count, 720);  // 6! = 720
-	}
-
-		@Test
-		public void list8Test() {
-			// With more than 10-12 it will probably hit an OutOfMemoryError.
-			List<String> items = new ArrayList<String>(3);
-			items.add("apple");
-			items.add("banana");
-			items.add("mango");
-			items.add("pear");
-			items.add("orange");
-			items.add("cherry");
-			items.add("kumquat");
-			items.add("lychee");
-		
-			Permutations<String> permutations = new Permutations<String>( items );
-		
-			int count = 0;
-			for ( List<String> permutation : permutations ) { 
-				count++;
-			}
-			
-			assertEquals(count, 40320);  // 8! = 40,320
-		}
-
-
-		@Test
-		public void list8OnDemandTest() {
-			// With more than 10-12 it will probably hit an OutOfMemoryError.
-			List<String> items = new ArrayList<String>(3);
-			items.add("apple");
-			items.add("banana");
-			items.add("mango");
-			items.add("pear");
-			items.add("orange");
-			items.add("cherry");
-			items.add("kumquat");
-			items.add("lychee");
-		
-			Permutations<String> permutations = new Permutations<String>( items );
-			permutations.generateOnDemand(true);
-		
-			int count = 0;
-			for ( List<String> permutation : permutations ) { 
-				count++;
-			}
-			
-			assertEquals(count, 40320);  // 8! = 40,320
-		}
-		
-		
-	@Test
-	public void list10Test() {
-		// With more than 10-12 it will probably hit an OutOfMemoryError.
-		List<String> items = new ArrayList<String>(3);
-		items.add("apple");
-		items.add("banana");
-		items.add("mango");
-		items.add("pear");
-		items.add("orange");
-		items.add("cherry");
-		items.add("kumquat");
-		items.add("lychee");
-		items.add("melon");
-		items.add("canteloupe");
-		/*
-		items.add("watermelon");
-		items.add("strawberry");
-		items.add("raspberry");
-		items.add("blueberry");
-		items.add("lemon");
-		items.add("lime");
-		items.add("blackberry");
-		items.add("grape");
-		items.add("avocado");
-		items.add("guanabana");
-		items.add("plantain");
-		items.add("coconut");
-		items.add("pineapple");
-		items.add("kiwifruit");  */
-	
-		Permutations<String> permutations = new Permutations<String>( items );
-	
-		int count = 0;
-		for ( List<String> permutation : permutations ) { 
-			count++;
-		}
-		
-		assertEquals(count, 3628800);  // 10! = 3628800
-	}
-
-	@Test
-	public void list10OnDemandTest() {
-		List<String> items = new ArrayList<String>(3);
-		items.add("apple");
-		items.add("banana");
-		items.add("mango");
-		items.add("pear");
-		items.add("orange");
-		items.add("cherry");
-		items.add("kumquat");
-		items.add("lychee");
-		items.add("melon");
-		items.add("canteloupe");
-		/*
-		items.add("watermelon");
-		items.add("strawberry");
-		items.add("raspberry");
-		items.add("blueberry");
-		items.add("lemon");
-		items.add("lime");
-		items.add("blackberry");
-		items.add("grape");
-		items.add("avocado");
-		items.add("guanabana");
-		items.add("plantain");
-		items.add("coconut");
-		items.add("pineapple");
-		items.add("kiwifruit");  */
-
-		Permutations<String> permutations = new Permutations<String>( items );
-		permutations.generateOnDemand(true);
-
-		int count = 0;
-		for ( List<String> permutation : permutations ) { 
-			count++;
-		}
-		
-		assertEquals(count, 3628800);  // 10! = 3628800
-	}
-
-
-	//@Test
-	public void list12OnDemandTestHide() {
-		// Too long to be part of manual unit test runs.
-		List<String> items = new ArrayList<String>(3);
-		items.add("apple");
-		items.add("banana");
-		items.add("mango");
-		items.add("pear");
-		items.add("orange");
-		items.add("cherry");
-		items.add("kumquat");
-		items.add("lychee");
-		items.add("melon");
-		items.add("canteloupe");
-		items.add("watermelon");
-		items.add("strawberry");
-/*		items.add("raspberry");
-		items.add("blueberry");
-		items.add("lemon");
-		items.add("lime");
-		items.add("blackberry");
-		items.add("grape");
-		items.add("avocado");
-		items.add("guanabana");
-		items.add("plantain");
-		items.add("coconut");
-		items.add("pineapple");
-		items.add("kiwifruit");
- */
-		Permutations<String> permutations = new Permutations<String>( items );
-		permutations.generateOnDemand(true);
-
-		long count = 0;
-		for ( List<String> permutation : permutations ) { 
-			count++;
-//			System.out.print(count + ".");
-		}
-
-		assertEquals(count, 479001600L);  // 12! = 479,001,600 // 14! = 87,178,291,200
-	}
-
-	
 	@Test
 	public void set4Test() {
 		Set<String> set = new HashSet<String>(4);
@@ -308,34 +74,94 @@ public class PermutationsTest {
 
 		int count = 0;
 		for ( List<String> permutation : permutations ) { 
+			// TODO Confirm that all permutations are different, even when an unordered set is provided and the permutations are calculated on-demand.
 			count++;
 		}
-		
 		assertEquals(count, 24);  // 4! = 24
 	}
 
 	@Test
-	public void set4OnDemandTest() {
-		Set<String> set = new HashSet<String>(4);
-		set.add("apple");
-		set.add("banana");
-		set.add("canteloupe");
-		set.add("durian");
+	public void list6Test() {
+		List<String> items = new ArrayList<String>(6);
+		items.add("apple");
+		items.add("banana");
+		items.add("mango");
+		items.add("pear");
+		items.add("orange");
+		items.add("cherry");
 
-		Permutations<String> permutations = new Permutations<String>( set ); 
-		permutations.generateOnDemand(true);
+		Permutations<String> permutations = new Permutations<String>( items );
 
 		int count = 0;
 		for ( List<String> permutation : permutations ) { 
-			// TODO Confirm that all permutations are different, even when an unordered set is provided and the permutations are calculated on-demand.
 			count++;
 		}
-
-		assertEquals(count, 24);  // 4! = 24
+		
+		assertEquals(count, 720);  // 6! = 720
 	}
 
 
-	
+		@Test
+		public void list8Test() {
+			List<String> items = new ArrayList<String>(3);
+			items.add("apple");
+			items.add("banana");
+			items.add("mango");
+			items.add("pear");
+			items.add("orange");
+			items.add("cherry");
+			items.add("kumquat");
+			items.add("lychee");
+		
+			Permutations<String> permutations = new Permutations<String>( items );
+		
+			int count = 0;
+			for ( List<String> permutation : permutations ) { 
+				count++;
+			}
+			
+			assertEquals(count, 40320);  // 8! = 40,320
+		}
+
+	@Test
+	public void list10Test() {
+		List<String> items = new ArrayList<String>(3);
+		items.add("apple");
+		items.add("banana");
+		items.add("mango");
+		items.add("pear");
+		items.add("orange");
+		items.add("cherry");
+		items.add("kumquat");
+		items.add("lychee");
+		items.add("melon");
+		items.add("canteloupe");
+		/*
+		items.add("watermelon");
+		items.add("strawberry");
+		items.add("raspberry");
+		items.add("blueberry");
+		items.add("lemon");
+		items.add("lime");
+		items.add("blackberry");
+		items.add("grape");
+		items.add("avocado");
+		items.add("guanabana");
+		items.add("plantain");
+		items.add("coconut");
+		items.add("pineapple");
+		items.add("kiwifruit");  */
+
+		Permutations<String> permutations = new Permutations<String>( items );
+
+		int count = 0;
+		for ( List<String> permutation : permutations ) { 
+			count++;
+		}
+		
+		assertEquals(count, 3628800);  // 10! = 3628800
+	}
+
 	@Test
 	public void set10Test() {
 		// With more than 10-12 it will probably hit an OutOfMemoryError.
@@ -376,9 +202,10 @@ public class PermutationsTest {
 		assertEquals(count, 3628800);  // 10! = 3628800
 	}
 
+
 	@Test
-	public void set10OnDemandTest() {
-		Set<String> items = new HashSet<String>();
+	public void listPartial25Test() {
+		List<String> items = new ArrayList<String>(3);
 		items.add("apple");
 		items.add("banana");
 		items.add("mango");
@@ -389,7 +216,6 @@ public class PermutationsTest {
 		items.add("lychee");
 		items.add("melon");
 		items.add("canteloupe");
-		/*
 		items.add("watermelon");
 		items.add("strawberry");
 		items.add("raspberry");
@@ -403,18 +229,21 @@ public class PermutationsTest {
 		items.add("plantain");
 		items.add("coconut");
 		items.add("pineapple");
-		items.add("kiwifruit");  */
-
+		items.add("kiwifruit");
+		items.add("peach");
+ 
 		Permutations<String> permutations = new Permutations<String>( items );
-		permutations.generateOnDemand(true);
 
 		int count = 0;
 		for ( List<String> permutation : permutations ) { 
 			count++;
+			// Fetch only the first 8! permutations, to test that performance is acceptable and there's no OOM errors.
+			if (count == 40320 ) { 
+				break;
+			}
 		}
-		
-		assertEquals(count, 3628800);  // 10! = 3628800
-	}
 
+		assertEquals(count, 40320); 
+	}
 
 }
